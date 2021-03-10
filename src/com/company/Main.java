fc5dac1d22efe507fefd95006ac002b58252c5d0
Danil <danil1198@yandex.ru>
2021-03-11 00:26:37 +0300

    Редактирование кода

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,20 +7,19 @@ import java.lang.Math;
 
 public class Main {
 
-    public static boolean Check(int num){
-        int number = Math.abs(num);
-        return Integer.bitCount(number) == 1;
-    }
-
     public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-        int a;
 	    System.out.println("Введите число: ");
 	    if (sc.hasNextInt()) {
-            a = sc.nextInt();
+            int a = sc.nextInt();
             System.out.println("Заданное число по абсолютной величине " + (Check(a)? "является":"не является") + " степенью двойки");
         } else {
 	        System.out.println("Вы ввели не число");
         }
+    }
+
+    public static boolean Check(int num){
+        int number = Math.abs(num);
+        return Integer.bitCount(number) == 1;
     }
 }
